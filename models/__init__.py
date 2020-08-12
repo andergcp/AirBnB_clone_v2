5df2d13be489671d8dc1c08a20ba8125f3e8cc0f
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-08-12 15:48:38 +0000

    [Remove] temporal file

--- models/__init__.py
+++ models/__init__.py
@@ -6,9 +6,7 @@ from os import environ
 if environ.get('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
-
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
