e18f0282f5701658c1565f3ecb67bb0447c8fd3e
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-08-13 22:31:23 +0000

    [Add]

--- models/place.py
+++ models/place.py
@@ -5,12 +5,11 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
 from os import environ
 
-if environ.get('HBNB_TYPE_STORAGE') == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id'), primary_key=True),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id'), primary_key=True))
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'), primary_key=True),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'), primary_key=True))
 
 
 class Place(BaseModel, Base):
