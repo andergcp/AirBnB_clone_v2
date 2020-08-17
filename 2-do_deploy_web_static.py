8566cd4797f314086764709cc30b42b36664a73e
andergcp <andergcp@hotmail.com>
2020-08-17 22:59:34 +0000

    [FIX] Pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,16 +17,18 @@ def do_deploy(archive_path):
     if not path.isfile(archive_path):
         return False
 
-
     file_name = archive_path.split('/')[-1]
     remote_path = "/tmp/{}".format(file_name)
 
     put(archive_path, remote_path)
     run('mkdir -p /data/web_static/releases/{}/'.format(file_name))
-    run('tar -xzf /tmp/{0} -C /data/web_static/releases/{0}/'.format(file_name))
+    run('tar -xzf /tmp/{0} -C\
+    /data/web_static/releases/{0}/'.format(file_name))
     run('rm /tmp/{}'.format(file_name))
-    run('mv /data/web_static/releases/{0}/web_static/* /data/web_static/releases/{0}/'.format(file_name))
+    run('mv /data/web_static/releases/{0}/web_static/* \
+    /data/web_static/releases/{0}/'.format(file_name))
     run('rm -rf /data/web_static/releases/{}/web_static'.format(file_name))
     run('rm -rf /data/web_static/current')
-    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(file_name))
+    run('ln -s /data/web_static/releases/{}/ \
+    /data/web_static/current'.format(file_name))
     print("New version deployed!\n")
