ff06dd73e93333467804263e64c9d36b047b9072
andergcp <andergcp@hotmail.com>
2020-09-01 23:39:22 +0000

    [TASK] Task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+'''
+starts a Flask web application
+'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def starts_app():
+    '''
+    return value
+    '''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_screen():
+    '''
+    return hbnb screen
+    '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    '''
+    return c + text
+    '''
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    '''
+    return Python + text(optional)
+    '''
+    return 'Python %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    '''
+    runs the function
+    '''
+    app.run(host='0.0.0.0', port=5000)
