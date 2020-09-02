b0f1d7c42fffd6b070499b46d96770fc514d387e
andergcp <andergcp@hotmail.com>
2020-09-02 20:22:25 +0000

    [FIX] Pycodstyle

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -29,7 +29,8 @@ def states_list():
         else:
             cities += state.cities()
 
-    return render_template('8-cities_by_states.html', states=states, cities=cities)
+    return render_template(
+        '8-cities_by_states.html', states=states, cities=cities)
 
 
 if __name__ == '__main__':
